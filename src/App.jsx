b6f92975f35f83633a4d12ec41d7b65704fc00cc
Your Name <your@email.com>
2025-12-17 18:57:54 +0300

    'second'

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import {  Routes, Route } from "react-router-dom";
 import Counter from "./Component/Counter";
+import Todo from "./Component/TodoList/Todo";
+import "./App.css";
 
 function App() {
   return (
-   
+   <div >
     
       <Routes>
         <Route path="/" element={<Counter />} />
+        <Route path="/todo" element={<Todo />} />
       </Routes>
+      </div>
    
   );
 }
---
+++ src/Component/TodoList/Todo.jsx
@@ -0,0 +1,66 @@
+import { list } from 'postcss'
+import React, { useState } from 'react'
+
+export default function Todo() {
+    const [input , setInput]= useState('')
+        const [desc , setdesc]= useState('')
+
+
+    const [lists,setList]=useState([
+        {
+            id:1,
+            task:'exercise',
+            description:"exercise for 20 mins"
+
+        },
+        {
+            id:2,
+            task:"code",
+            description:"regularly make atleast 1 project"
+
+
+        },
+        {
+            id:3,
+            task:"eat healthy",
+            description:"eat healty food and be on balanced diet"
+
+        }
+    ])
+const handleAddTask=()=>{
+    if (input.trim()===""|| desc===""){
+        alert('add task and desc')
+        return;
+    }
+    const newtask ={
+        id: lists.length +1,
+        task : input,
+        descriptio: desc
+    }
+    setList([...lists,newtask])
+    setInput('')
+    setdesc('')
+}
+const handledelete=(id)=>{
+    const updatedtask = lists.filter((task)=> task.id != id)
+    setList(updatedtask)
+}
+  return (
+    <div className='text-black'>
+        <div>
+            <input type="text" value={input} name='input' placeholder='enter your task' onChange={(e)=>setInput(e.target.value)} />
+            <textarea name='description'value={desc} placeholder='enter the description' onChange={(e)=>setdesc(e.target.value)}/>
+            <button onClick={handleAddTask}>add</button>
+        </div>
+    {lists.map((m)=>(
+        <div key={m.id}>
+            <h1>{m.task}</h1>
+            <p>{m.description}</p>
+            <button onClick={()=>handledelete(m.id)}> X</button>
+        </div>
+    ))}
+    </div>
+    
+
+  )
+}
